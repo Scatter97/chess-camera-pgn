d60271e9914d6cf0fbb165bec54c3f21749fe395
Scatter97 <133073535+Scatter97@users.noreply.github.com>
2026-07-30 21:48:01 -0400

    Launch Rev. 35 with Chess960 generator on Windows

--- run_windows.bat
+++ run_windows.bat
@@ -5,5 +5,5 @@ if not exist ".venv\Scripts\python.exe" (
     .venv\Scripts\python.exe -m pip install --upgrade pip
     .venv\Scripts\python.exe -m pip install -r requirements.txt
 )
-.venv\Scripts\python.exe revision35_final.py %*
+.venv\Scripts\python.exe revision35_chess960.py %*
 pause
