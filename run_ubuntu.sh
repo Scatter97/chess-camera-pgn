ba303a9d4dc6a18baa244f1b69c5d9ee4e76a6d6
Scatter97 <133073535+Scatter97@users.noreply.github.com>
2026-07-30 21:57:05 -0400

    Launch expanded Rev. 35 feature menu on Ubuntu

--- run_ubuntu.sh
+++ run_ubuntu.sh
@@ -15,4 +15,4 @@ if [[ ! -x ".venv/bin/python" ]]; then
     .venv/bin/python -m pip install -r requirements.txt
 fi
 
-exec .venv/bin/python revision35_chess960.py "$@"
+exec .venv/bin/python revision35_features.py "$@"
