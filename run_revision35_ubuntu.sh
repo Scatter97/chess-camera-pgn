be0234d815a03cfe7665492908e0a3d20b4b61cd
Scatter97 <133073535+Scatter97@users.noreply.github.com>
2026-07-30 21:57:23 -0400

    Point Revision 35 Ubuntu launcher to expanded feature menu

--- run_revision35_ubuntu.sh
+++ run_revision35_ubuntu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 cd "$(dirname "$0")"
-if [ -x .venv/bin/python ]; then .venv/bin/python revision35_chess960.py "$@"; else python3 revision35_chess960.py "$@"; fi
+if [ -x .venv/bin/python ]; then .venv/bin/python revision35_features.py "$@"; else python3 revision35_features.py "$@"; fi
